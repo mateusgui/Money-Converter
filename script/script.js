006add84a3f5e613793c087894ef6a1769178adb
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-11 12:18:10 -0400

    update script.js
    Consulta à API para valores em tempo real

--- script/script.js
+++ script/script.js
@@ -4,18 +4,41 @@ const currencySelector = document.querySelector(".currency-selector")
 const currencyImage = document.querySelector(".convert-to")
 const currencyText = document.querySelector(".to-type")
 
-function convertValues() {
+const convertValues = async () => {
     const input = document.querySelector(".input-value").value
     const htmlValueToConvert = document.querySelector(".from-quantity")
     const htmlValueConverted = document.querySelector(".to-quantity")
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    //Valores padrão caso a consulta à API não funcione
+    let dolarToday = 5.2 
+    let euroToday = 6.2
 
-    if(selector.value == "USD") {
+    if (selector.value == "USD") {
+        //Consulta do valor do Dólar
+        try {
+            const dolarApi = await fetch("https://economia.awesomeapi.com.br/json/USD-BRL")
+            const dolarData = await dolarApi.json()
+            dolarToday = dolarData[0].high
+
+            console.log(dolarToday)
+        } catch (error) {
+            console.error({ error: error.message })
+        }
+
         htmlValueConverted.innerHTML = new Intl.NumberFormat('en-us', { style: 'currency', currency: 'USD' }).format(input / dolarToday)
     }
-    if(selector.value == "EUR") {
+    if (selector.value == "EUR") {
+        //Consulta do valor do Euro
+        try {
+            const euroApi = await fetch("https://economia.awesomeapi.com.br/json/EUR-BRL")
+            const euroData = await euroApi.json()
+            euroToday = euroData[0].high
+
+            console.log(euroToday)
+        } catch (error) {
+            console.error({ error: error.message })
+        }
+
         htmlValueConverted.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(input / euroToday)
     }
 
@@ -23,11 +46,11 @@ function convertValues() {
 }
 
 function changeImage() {
-    if(selector.value == "USD"){
+    if (selector.value == "USD") {
         currencyImage.src = "img/dolar-icon.png"
         currencyText.innerHTML = "Dólar"
     }
-    if(selector.value == "EUR") {
+    if (selector.value == "EUR") {
         currencyImage.src = "img/euro-icon.png"
         currencyText.innerHTML = "Euro"
     }
